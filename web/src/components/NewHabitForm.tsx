2a2bf191fdd23b87047878c09483c75697edac25
amanda-santos <amandasf0200@gmail.com>
2023-01-20 14:47:39 -0300

    feat: ✨ add new habit form and habit day popover

---
+++ web/src/components/NewHabitForm.tsx
@@ -0,0 +1,31 @@
+import { Check } from "phosphor-react";
+
+export const NewHabitForm = () => {
+  return (
+    <form className="w-full flex flex-col mt-6">
+      <label htmlFor="title" className="font-semibold leading-tight">
+        What's your goal?
+      </label>
+
+      <input
+        type="text"
+        id="title"
+        placeholder="e.g. exercise, read a chapter, sleep well, etc..."
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        autoFocus
+      />
+
+      <label htmlFor="" className="font-semibold leading-tight mt-4">
+        What is the recurrence?
+      </label>
+
+      <button
+        type="submit"
+        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-violet-600 hover:bg-violet-500"
+      >
+        <Check size={20} weight="bold" />
+        Confirm
+      </button>
+    </form>
+  );
+};
---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,19 @@
+interface ProgressBarProps {
+  progress: number;
+}
+
+export const ProgressBar = (props: ProgressBarProps) => {
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div
+        role="progressbar"
+        aria-label="Progress of habits completed that day"
+        aria-valuenow={props.progress}
+        className="h-3 rounded-xl bg-violet-600"
+        style={{
+          width: `${props.progress}%`,
+        }}
+      />
+    </div>
+  );
+};
